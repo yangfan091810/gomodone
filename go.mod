f31c9f690e5e804a102a5d27e6da46ed9b7a7833
yang.fan <154055342@qq.com>
2021-09-18 14:47:01 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yangfan091810/gomodone
+
+go 1.17
---
+++ say.go
@@ -0,0 +1,8 @@
+package gomodone
+
+import "fmt"
+
+// SayHi say hi to someone
+func SayHi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
